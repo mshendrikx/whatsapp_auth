bd879bc877296572ac578442e856fae5077127bb
Maurício Servatius Hendrikx <mauricio.servatius@gmail.com>
2025-07-22 12:12:59 -0300

    main

--- project/whatsapp_api.py
+++ project/whatsapp_api.py
@@ -23,10 +23,20 @@ def whatsapp_send_message(base_url, api_key, session, contacts, content, content
     contatc_fail = []
     for contact in contacts:        
         chatid = [whatsapp_convert_phone(contact)]
-        json_data = {'chatId': chatid,
-                 "contentType": content_type,
-                 'content': content
-                }
+        if content_type == "string":
+            json_data = {'chatId': chatid,
+                         'contentType': content_type,
+                         'content': content
+                        }
+        elif content_type == "MessageMedia":
+            json_data = {'chatId': chatid,
+                         'contentType': content_type,
+                         'content': {
+                            'mimetype': "image/jpeg",
+                            'data': content,
+                            "filename": "image.jpg"
+                            }
+                        }
         try:
             response = requests.post(url=url, headers=headers, json=json_data)
             if response.status_code != 200:
